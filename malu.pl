0bc27c5bee201efe0d509cf70e3e5a7120b48310
sebbu <zsbe17fr@yahoo.fr>
2013-05-01 20:19:19 +0200

    Perl plugin

---
+++ malu.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl -w
+
+package IRC::Xchat::MALu;
+
+IRC::register("xchat malu", "0.1.0", "", "");
+
+sub malu1 {
+	open FILE, "<", "C:\\Documents and Settings\\sebbu\\Application Data\\X-Chat 2\\malu1.txt" or die $!;
+	my $line = <FILE>;
+	if(length $line > 0) {
+		IRC::command($line);
+	}
+	else {
+		IRC::print('no anime currently playing.');
+	}
+	close FILE;
+	return 1;
+}
+
+sub malu2 {
+	open FILE, "<", "C:\\Documents and Settings\\sebbu\\Application Data\\X-Chat 2\\malu2.txt" or die $!;
+	my $line = <FILE>;
+	if(length $line > 0) {
+		IRC::command($line);
+	}
+	else {
+		IRC::print('no anime currently playing.');
+	}
+	close FILE;
+	return 1;
+}
+
+sub malu3 {
+	open FILE, "<", "C:\\Documents and Settings\\sebbu\\Application Data\\X-Chat 2\\malu3.txt" or die $!;
+	my $line = <FILE>;
+	if(length $line > 0) {
+		IRC::command($line);
+	}
+	else {
+		IRC::print('no anime currently playing.');
+	}
+	close FILE;
+	return 1;
+}
+
+sub malu4 {
+	open FILE, "<", "C:\\Documents and Settings\\sebbu\\Application Data\\X-Chat 2\\malu4.txt" or die $!;
+	my $line = <FILE>;
+	if(length $line > 0) {
+		IRC::command($line);
+	}
+	else {
+		IRC::print('no anime currently playing.');
+	}
+	close FILE;
+	return 1;
+}
+
+IRC::add_command_handler("malu" ,"IRC::Xchat::MALu::malu1");
+IRC::add_command_handler("malu2","IRC::Xchat::MALu::malu2");
+IRC::add_command_handler("malu3","IRC::Xchat::MALu::malu3");
+IRC::add_command_handler("malu4","IRC::Xchat::MALu::malu4");
+IRC::print('Loaded malu script by sebbu.');
