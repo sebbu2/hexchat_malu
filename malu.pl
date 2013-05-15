3ed4593e2d4b01ac8f7bb950104f783b68a2feab
sebbu <zsbe17fr@yahoo.fr>
2013-05-15 20:45:04 +0200

    Added /malu0 for preview

--- malu.pl
+++ malu.pl
@@ -2,7 +2,20 @@
 
 package IRC::Xchat::MALu;
 
-IRC::register("xchat malu", "0.1.0", "", "");
+IRC::register("xchat malu", "0.1.1", "", "");
+
+sub malu0 {
+	open FILE, "<", "C:\\Documents and Settings\\sebbu\\Application Data\\X-Chat 2\\malu1.txt" or die $!;
+	my $line = <FILE>;
+	if(length $line > 0) {
+		IRC::print($line);
+	}
+	else {
+		IRC::print('no anime currently playing.');
+	}
+	close FILE;
+	return 1;
+}
 
 sub malu1 {
 	open FILE, "<", "C:\\Documents and Settings\\sebbu\\Application Data\\X-Chat 2\\malu1.txt" or die $!;
@@ -56,6 +69,7 @@ sub malu4 {
 	return 1;
 }
 
+IRC::add_command_handler("malu0","IRC::Xchat::MALu::malu0");
 IRC::add_command_handler("malu" ,"IRC::Xchat::MALu::malu1");
 IRC::add_command_handler("malu2","IRC::Xchat::MALu::malu2");
 IRC::add_command_handler("malu3","IRC::Xchat::MALu::malu3");
